74deb7d8bf2c128610671774df41465d25f4a2ab
abdulchakam <muhammadabdulchakam@gmail.com>
2020-03-28 22:43:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mysqlaplikasi2/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.mysqlaplikasi2.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.70.10/PemogramanMobile2/APIBarang/tampil_data.php";
+    public static final String URL_INSERT = "http://192.168.43.206/PemogramanMobile2/APIBaranag/tambah_data.php";
+    public static final String URL_DELETE = "http://192.168.43.206/PemogramanMobile2/APIBaranag/delete_data.php";
+    public static final String URL_UPDATE = "http://192.168.43.206/PemogramanMobile2/APIBaranag/edit_data.php";
+}
